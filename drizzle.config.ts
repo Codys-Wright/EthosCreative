b88b70b5f1a7fcb50aa78fa9cd29e1cf84277fc9
FastTrackAudio <acodywright@gmail.com>
2025-02-25 11:56:04 -0800

    adds drizzle config and database scripts/ some auth stuff

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import { type Config } from "drizzle-kit";
 import { env } from "./env";
 
 export default {
-  schema: "@/lib/db/schema.prisma",
+  schema: "./lib/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
   },
-  tablesFilter: ["start-bundle_*"],
+  // tablesFilter: [""]
 } satisfies Config;
