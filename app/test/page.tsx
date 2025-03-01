9664969e3689e67f3b778de96635c24190c0361e
FastTrackAudio <acodywright@gmail.com>
2025-03-01 13:06:58 -0800

    chore: remove test files

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,12 +1,11 @@
 import { ExampleComponent } from "@/features/example/presentation";
-import { TestQuery } from "@/features/example/tanstack/testQuery";
+
 
 export default function TestPage() {
   return (
     <div className="container py-8">
       <h1 className="text-3xl font-bold mb-8">Database Operations Test Page</h1>
-    
-      <TestQuery />
+      <ExampleComponent />
     </div>
   );
 }
--- app/test/query/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TestQuery } from "@/features/example/tanstack/testQuery";
-
-export default function TestQueryPage() {
-  return (
-    <div className="flex flex-row gap-4 p-10 justify-center items-center">
-      <TestQuery />
-    </div>
-  );
-}
