137fdcb6c4c69b82c7e8c9bd17db536797b4f33f
Cody Wright <acodywright@gmail.com>
2025-12-23 07:13:43 +0000

    wrap in server function

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,17 +7,20 @@ import { serverRuntime } from "@/features/core/server";
 import { TodosService } from "@/features/todo/server/todos-service";
 import { App } from "./-index/app";
 import { todosAtom } from "@/features/todo/client";
+import { createServerFn } from "@tanstack/react-start";
+
+const getTodos = createServerFn().handler(async () => {
+  const todos = await serverRuntime.runPromiseExit(
+    Effect.gen(function* () {
+      const service = yield* TodosService;
+      return yield* service.list;
+    }),
+  );
+  return dehydrate(todosAtom.remote, Result.fromExit(todos));
+});
 
 export const Route = createFileRoute("/")({
-  loader: async () => {
-    const todos = await serverRuntime.runPromiseExit(
-      Effect.gen(function* () {
-        const service = yield* TodosService;
-        return yield* service.list;
-      }),
-    );
-    return dehydrate(todosAtom.remote, Result.fromExit(todos));
-  },
+  loader: () => getTodos(),
   component: AppWrapper,
 });
 
